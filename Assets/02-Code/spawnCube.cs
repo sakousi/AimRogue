7e2d72d5f5dceabfc6b9501108ae85ce46192a5b
QuillG <quentin.guillemand@gmail.com>
2026-02-24 14:41:02 +0100

    init folders

---
+++ Assets/02-Code/spawnCube.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class spawnCube : MonoBehaviour
+
+{
+    public GameObject cubePrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnPrefab", 1f, 1f);
+        // stop spawning after 10 seconds
+        Invoke("StopSpawning", 30f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnPrefab()
+    {
+        GameObject cube = Instantiate((cubePrefab));
+        cube.transform.position = new Vector3(Random.Range(-10, 10), 50, Random.Range(-10, 10));
+    }
+
+    public void StopSpawning()
+    {
+        CancelInvoke("SpawnPrefab");
+    }
+}
